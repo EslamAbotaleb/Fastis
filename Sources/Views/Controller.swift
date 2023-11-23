8e9d787760a4b81e99138841c3a24cad447da1e7
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2023-11-23 17:55:13 +0200

    remove function reload data calendar

--- Sources/Views/Controller.swift
+++ Sources/Views/Controller.swift
@@ -674,14 +674,14 @@ open class FastisController<Value: FastisValue>: UIViewController, JTACMonthView
     ) {
         if (dayNumber != nil) {
             dateSelected = date
-            let componentDate = getDate(from: dayNumber ?? 0, fromDate: dateSelected ?? Date()).get(.day, .month, .year)
+            let componentDate = getDate(from: dayNumber ?? 0, fromDate: dateSelected!).get(.day, .month, .year)
 //            if let day = components.day, let month = components.month, let year = components.year {
 //                maximumDate = Calendar(identifier: .gregorian).date(from: DateComponents(year: year, month: month, day: day))
 //            }
 //            let components = dateSelected?.get(.day, .month, .year)
 //            if let day = components?.day, let _ = components?.month, let _ = components?.year {
                 maximumDate = Calendar(identifier: .gregorian).date(from: DateComponents(year: componentDate.year, month: componentDate.month, day: componentDate.day))
-            calendar.reloadData()
+      
 //            }
         }
 
