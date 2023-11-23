f020268ce037f12126dbfbf082e71bb592638949
Eslam  Baioumy <e.baiomy@mobily.com.sa>
2023-11-23 16:37:15 +0200

    add static value for month

--- Sources/Views/Controller.swift
+++ Sources/Views/Controller.swift
@@ -678,6 +678,7 @@ open class FastisController<Value: FastisValue>: UIViewController, JTACMonthView
             if let day = components?.day, let month = components?.month, let year = components? .year {
                 print("day: \(day), month: \(month), year: \(year)")
 //                maximumDate = Calendar(identifier: .gregorian).date(from: DateComponents(year: year, month: getMonths(from: dayNumber ?? 0), day: dayNumber))
+                maximumDate = Calendar(identifier: .gregorian).date(from: DateComponents(year: year, month: 2, day: dayNumber))
                 calendarView.reloadData()
             }
         }
